65cccba0204299d43a52394a55eb2eed7471e9f4
snehasivadas21 <snehasivadas2002@gmail.com>
2026-03-28 15:21:30 +0530

    fourth commit

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DATABASE"),
-        'USER': 'root',
-        'PASSWORD': os.getenv("MYSQL_ROOT_PASSWORD"),
-        'HOST': os.getenv("MYSQL_HOST", "db"),
-        'PORT': os.getenv("MYSQL_PORT", "3306"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
