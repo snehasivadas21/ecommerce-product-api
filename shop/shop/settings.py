ed2e6edfa09797ebce17448a0bc5944be692b94c
snehasivadas21 <snehasivadas2002@gmail.com>
2026-03-28 12:49:49 +0530

    third commit

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -81,10 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv("MYSQL_DATABASE"),
-        'USER': os.getenv("MYSQL_USER"),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-        'HOST': os.getenv("MYSQL_HOST"),
-        'PORT': os.getenv("MYSQL_PORT"),
+        'USER': 'root',
+        'PASSWORD': os.getenv("MYSQL_ROOT_PASSWORD"),
+        'HOST': os.getenv("MYSQL_HOST", "db"),
+        'PORT': os.getenv("MYSQL_PORT", "3306"),
     }
 }
 
